06ce571b8601c114d8c10a0e522c89c818eccd50
Ivan Pustogarov <ivan@ipust.net>
2021-12-09 02:44:20 -0500

    Add ARM_PTDUMP config options to ./configure.sh script

--- evasion-kernels/patches/4.9.117/configure.sh
+++ evasion-kernels/patches/4.9.117/configure.sh
@@ -3,3 +3,4 @@
 export PATH="$PATH:$(realpath ../../compilers/arm-linux-androideabi-4.9/bin)"
 make ARCH=arm CROSS_COMPILE=arm-linux-androideabi- vexpress_defconfig
 sed -i 's/# CONFIG_DEVTMPFS_MOUNT is not set/CONFIG_DEVTMPFS_MOUNT=y/g'  .config
+sed -i 's/# CONFIG_ARM_PTDUMP is not set/CONFIG_ARM_PTDUMP=y/g'  .config
